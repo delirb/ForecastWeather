c03896f06390f8626f59034cf9772f366796d257
delirb <49633517+delirb@users.noreply.github.com>
2023-03-11 01:14:44 +0330

    icon

--- src/script.js
+++ src/script.js
@@ -1,9 +1,13 @@
-function formatDate(date) {
+function formatDate(dateinfo) {
+  let date = new Date(dateinfo);
+  console.log(date);
   let hours = date.getHours();
+  let minutes = date.getMinutes();
+
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = date.getMinutes();
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
@@ -37,12 +41,21 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", `  ${response.data.weather[0].description}`);
 }
-
+let city = "sydney";
 let Appkey = "984f5a7c489eb91ea3f8c3eb812a5080";
-let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=Isfahan&appid=${Appkey}&units=metric`;
+let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Appkey}&units=metric`;
 axios.get(weatherUrl).then(displayWeatherCondition);
-
-let currentTime = new Date();
-document.querySelector("#date").innerHTML = formatDate(currentTime);
-console.log(formatDate(currentTime));
